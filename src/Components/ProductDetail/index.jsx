19e8af2e1f4bb1346c3110e38a0e493c6c97fed7
Naya070 <nayarith070@gmail.com>
2023-07-22 12:45:21 -0400

    Se agregó productdetail

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,22 @@
+
+import "./styles.css"
+
+
+
+
+
+
+
+function ProductDetail() {
+    return (
+        <aside className='flex flex-col fixed right-0 border bg-white border-black rounded-lg top-[68px] w-[360px] h-[calc(100vh-68px)]'>
+            <div className="flex justify-between items-center p-6">
+                <h2 className="font-medium text-xl">Detail</h2>
+                <div>x</div>
+            </div>
+        </aside>
+        
+    )
+}
+
+export default ProductDetail
